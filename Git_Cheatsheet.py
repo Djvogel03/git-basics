0b08bc3a73effc85374d4e7fba86925b97144a3f
djvogel03 <djvogel03@gmail.com>
2016-12-08 17:17:00 -0600

    Added cheatsheet to directory
    Addition of cheatsheet that now includes info on issue I was having w/ commit being aborted before I ever had a chance to enter the commit message.

---
+++ Git_Cheatsheet.py
@@ -0,0 +1,5 @@
+#issue w/ commit being aborted right away b/c of message being opened in sublime
+#Correction w/ :
+
+git config --global core.editor "'C:\Program Files\Sublmie Text 2\sublime_text.exe' -m -w"
+
